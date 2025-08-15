34d10fc8597044b1458027d381d38a0fc7a2fc60
Abhipaul11 <108946149+Abhipaul11@users.noreply.github.com>
2025-08-15 20:25:27 +0530

    first commit

---
+++ controllers/products/getallproduct.controller.js
@@ -0,0 +1,31 @@
+const Product = require("../../models/productSchema.models")
+const SERVER_ENDPOINT = process.env.SERVER_ENDPOINT
+
+const getAllproduct = async (req, res) => {
+    try {
+        const { size } = req.query;
+
+        const limit = size ? Number(size) : 100;
+
+        // if (size) query.size = Number(size)
+
+
+        const getproducts = await Product.find().sort({ createdAt: 1 }).populate("category").limit(limit)
+        if (!getproducts) {
+            return res.status(401).json({ msg: "no products in collection" })
+        }
+
+        const allProducts = getproducts.map((product) => {
+
+            return {
+                ...product.toObject(), image: `${SERVER_ENDPOINT}uploads/${product.image}`
+            }
+        })
+
+        return res.status(200).json({ msg: "There are your items", allProducts })
+    } catch (error) {
+        console.log("catch err", error)
+    }
+}
+
+module.exports = getAllproduct;
